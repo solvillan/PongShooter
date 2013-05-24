0125c34ccaa74d7a73fba12f279dce4740039c63
Rickard Doverfelt <rickard.doverfelt@gmail.com>
2013-05-24 19:18:26 +0200

    Fixed Setup

---
+++ src/se/doverfelt/pongShooter/Start.java
@@ -0,0 +1,16 @@
+package se.doverfelt.pongShooter;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Rickard
+ * Date: 2013-05-24
+ * Time: 19:15
+ * To change this template use File | Settings | File Templates.
+ */
+public class Start {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+    }
+
+}
